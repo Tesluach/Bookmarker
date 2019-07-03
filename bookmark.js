f5b95dbb112f509e0bd888e9bc826f5ac9f68219
Tes <tylersonluiz@gmail.com>
2019-07-03 22:01:09 +0300

    some changes made

--- bookmark.js
+++ bookmark.js
@@ -1,118 +1,106 @@
-var addBookmarks = document.forms['userInput'];
+
 var list = document.querySelector('.output ul');
 document.querySelector('.output').style.display = 'none';
 
 var siteNameValue = document.getElementById('siteName');
 var siteUrlValue = document.getElementById('siteUrl');
 
-
+var addBookmarks = document.forms['userInput'];
 addBookmarks.addEventListener('submit', function(e) {
     e.preventDefault();
-    var siteNameValue = document.getElementById('siteName').value;
-    var siteUrlValue = document.getElementById('siteUrl').value;
-    addBookmarks.querySelector('#siteName').value = "";
-    addBookmarks.querySelector('#siteUrl').value = "";
-
-   
-
-    
 
 
-    if (siteNameValue !== "" && siteUrlValue !== "") {
-        document.querySelector('.output').style.display = 'block';
-        // create elements 
-
-        var li = document.createElement('li');
-        var siteName = document.createElement('span');
-        var deleteSpan = document.createElement('span');
-        var linkSpan = document.createElement('span');
-        linkSpan.innerHTML = '<a href=' + siteUrlValue + '>View</a>';
-
-
-        //classes
-        siteName.classList.add('site');
-        deleteSpan.classList.add('delete');
-        linkSpan.classList.add('visit');
-
-        //text content
-        siteName.textContent = siteNameValue;
-        deleteSpan.textContent = 'Delete';
-
-
-        //append elements
-        list.appendChild(li);
-        li.appendChild(siteName);
-        li.appendChild(deleteSpan);
-        li.appendChild(linkSpan);
-
-
-    } else {
-        alert('Please fill all the inputs');
+    if (!siteNameValue.value && !siteUrlValue.value) {
+        alert('Please fill in the form.');
+        return false;
     }
-
-    // var expression = /[-a-zA-Z0-9@:$_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:$_\+.~#?&//=]*)?/gi;
-    // var regex = new RegExp(expression);
-    // if (!siteUrlValue.match(regex)) {
-    //     alert('Please use a valid URL');
-    //     return false;
-    // }
-
-});
-
-
-
-
-
-// deleting the list items
-
-list.addEventListener('click', function(e) {
-    // e.preventDefault();
-    if (e.target.className == 'delete') {
-        var lis = e.target.parentElement;
-        list.removeChild(lis);
-    }
-});
-
-
-
-/*       
         
-         //Working with the localStorage
-    
-    var bookmark = {
-        name : siteNameValue,
-        url : siteUrlValue
-    }
-    
-    if(localStorage.getItem('bookmarks') === null){
+        
+        var bookmark = {
+            name : siteNameValue.value,
+            url : siteUrlValue.value
+        }
+        
+        
+          // local storage---
+
+    if (localStorage.getItem('bookmarks') === null) {
         //init array
         var bookmarks = [];
         //add to array
         bookmarks.push(bookmark);
-        //set to localStorage
+        //set to localstorage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-       }
-    else{
-        //get bookmarks from localStorage
+
+    } else {
+        //get bookmarks from localstorage
         var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
         //add bookmark to array
         bookmarks.push(bookmark);
-        //Re-set back to localStorage
+        //re-set back to localstorage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        
     }
-    
+       fetchBookmarks(); 
 
-        //get bookmarks from localStorage
+   
+
+});
+
+
+
+function fetchBookmarks(){
+    document.querySelector('.output').style.display = 'block';
+        //get bookmarks from localstorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    list.innerHTML = '';
+    for(var i = 0; i < bookmarks.length; i++){
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+        
+        list.innerHTML +=  '<li>' + 
+                    '<span class="site">'+name+'</span>' +
+                    '<span class="delete">delete</span>' +
+                    '<span class="visit"><a href="'+url+'">View</a></span>' +
+                    '</li>';
+        
+    }
+}
+
+ fetchBookmarks();
+
+
+// deleting the list items
+
+list.addEventListener('click', deleting);
+
+function deleting(e){
+   
+      e.preventDefault();
+    
+    if (e.target.className == 'delete') {
         var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    
-        //get output id
-        var bookmarksOutput = document.querySelector('.output');
+            for (let i = 0; i < bookmarks.length; i++) {
+                if (bookmarks[i].name) {
+                    bookmarks.splice(i, 1);
+                }
+            }
+            localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        var lis = e.target.parentElement;
+        list.removeChild(lis);
+    }
+}
 
-        //build output
-        
-        for(var i = 0; i < bookmarks.length; i++){
-            var name =  bookmarks[i].name;
-            var url = bookmarks[i].url;
-        }
-        
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
